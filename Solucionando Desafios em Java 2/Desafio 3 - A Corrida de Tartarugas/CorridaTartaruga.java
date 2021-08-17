0d4dfd2dc7124670bc7220e2a383105204476c8c
HeMarchioni <henrique.marchioni@fatec.sp.gov.br>
2021-08-17 02:39:00 -0300

    Solucionando Desafios em Java - exercício 3 -  A Corrida de Tartarugas

---
+++ Solucionando Desafios em Java 2/Desafio 3 - A Corrida de Tartarugas/CorridaTartaruga.java
@@ -0,0 +1,46 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/*
+Solucionando Desafios em Java
+
+Exercicio 3 -  A Corrida de Tartarugas
+
+Realizado por Henrique Marchioni 17/08/2021
+
+ */
+
+
+public class CorridaTartaruga {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        //continue a solucao de acordo com o solicitado
+
+
+        do {
+           Integer.parseInt(br.readLine());
+            String str = br.readLine();
+
+            List<Integer> ints = Arrays.stream(str.split(" "))
+                    .map(Integer::parseInt)
+                    .collect(Collectors.toList());
+
+           int n = Collections.max(ints);
+
+            if (n > 10) {
+                if (n >=20) {
+                    System.out.println(3);
+
+                }else
+                System.out.println(2);
+
+            }else
+            System.out.println(1);
+        }
+        while (br.ready());
+    }
+}
